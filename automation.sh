ee0ee7d31042f18832b4daa0a440e576d1ee2529
amandeep-singh-vst-au4 <54429261+amandeep-singh-vst-au4@users.noreply.github.com>
2021-12-06 22:36:22 +0530

    Automation-v0.1.1

--- automation.sh
+++ automation.sh
@@ -1,5 +1,21 @@
 sudo apt update -y
 
-sudo systemctl status apache2
+if [ $(/etc/init.d/sudo systemctl status apache2 | grep -v grep | grep 'Apache2 is installed' | wc -l) > 0 ]
+then
+         echo "apche2 is installed"
+ else
+           sudo apt -get install apache2
+   fi
+   
+
+
+servstat=$(service apache2 status)
+
+if [[ $servstat == *"active (running)"* ]]; then
+          echo "process is running"
+  else
+          sudo /etc/init.d/apache2 restart
+          echo "service started again"
+  fi
 
 
