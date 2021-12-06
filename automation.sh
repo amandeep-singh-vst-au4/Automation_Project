d1bf6b84c2b8398212f656fb11af3a8c50618d11
amandeep-singh-vst-au4 <54429261+amandeep-singh-vst-au4@users.noreply.github.com>
2021-12-06 22:29:52 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -1,11 +1,6 @@
 sudo apt update -y
 
-if [ $(/etc/init.d/sudo systemctl status apache2 | grep -v grep | grep 'Apache2 is installed' | wc -l) > 0 ]
-then
-         echo "apche2 is installed"
- else
-           sudo apt -get install apache2
-   fi
+
 
 
  
